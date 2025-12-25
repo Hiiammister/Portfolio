d0c2fd8ad3de6ef7175c6b80a06df0de0f232b2c
Aditya Borkar <adityaborkar@Adityas-MacBook-Air.local>
2025-12-25 13:10:35 +0530

    Home_page_in_progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import {Canvas} from '@react-three/fiber'
+{/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div>*/}
 const Home = () => {
   return (
-    <div>
+    <section className="w-full h-screen relative">
       
-    </div>
+
+      
+
+      
+    </section>
   )
 }
 
