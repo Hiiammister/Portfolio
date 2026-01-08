829b1656dee9e934ffe23162a05cd5dc79e879c5
Aditya Borkar <adityaborkar@Adityas-MacBook-Air.local>
2026-01-08 15:06:29 +0530

    Formatted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route,BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import {Home, About, Projects, Contact} from './pages'
 
 const App = () => {
