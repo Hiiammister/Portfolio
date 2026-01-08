a9f0e91fb3e6b5870441b9d109698dcc85500277
Aditya Borkar <adityaborkar@Adityas-MacBook-Air.local>
2026-01-08 18:31:55 +0530

    fixing_bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDom from 'react-dom/client';
-import App from './App.jsx';
+import App from './app.jsx';
 import './index.css'
 
 ReactDom.createRoot(document.getElementById('root')).render(<App/>)
