e547d32b7e738a3b774525277715dea709c35f9b
Aditya Borkar <adityaborkar@Adityas-MacBook-Air.local>
2026-01-09 14:07:38 +0530

    added flex to mobile fixed2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,8 +18,9 @@ const Navbar = () => {
     `
   }
 >
-  AB
+  <p className="text-linear-gradient">AB</p>
 </NavLink>
+
 
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink to="/about" className={({isActive})=>isActive ? 'text-blue-500': 'text-black-500'}>
